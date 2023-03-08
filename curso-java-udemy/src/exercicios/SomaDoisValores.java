d7d6a7b95cc847a55372f5b092d33d336bdb2746
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-08 15:37:59 -0300

    somando valores

---
+++ curso-java-udemy/src/exercicios/SomaDoisValores.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class SomaDoisValores {
+	public static void main(String[] args) {
+		
+		//Faça um programa para ler dois valores inteiros, 
+		//e depois mostrar na tela a soma desses números
+		Scanner sc = new Scanner(System.in);
+		
+		int numero1, numero2;
+		
+		numero1 = sc.nextInt();
+		numero2 = sc.nextInt();
+		int soma = numero1 + numero2;
+		System.out.printf("SOMA = %d", soma);
+		
+		sc.close();
+	}
+}
