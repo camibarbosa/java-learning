44518be02d24c29413c5f19e3df6dd85ccc96efb
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-08 16:15:02 -0300

    solução do exercicio 4, somando salario

---
+++ curso-java-udemy/src/exercicios/Exercicio4.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio4 {
+	public static void main(String[] args) {
+		/*Fazer um programa que leia o número de um funcionário, 
+		 * seu número de horas trabalhadas, o valor que recebe por hora 
+		 * e calcula o salário desse funcionário. A seguir, mostre o número e 
+		 * o salário do funcionário, com duas casas decimais.*/
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		int numeroFuncionario = sc.nextInt();
+		int numeroHorasTrabalhadas = sc.nextInt();
+		double salario = sc.nextDouble();
+		double somaSalario = numeroHorasTrabalhadas * salario;
+		System.out.printf("NUMBER = %d%n", numeroFuncionario);
+		System.out.printf("SALARY = U$ %.2f", somaSalario);
+	
+		sc.close();
+	}
+}
