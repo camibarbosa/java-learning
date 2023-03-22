1a8c825ffccd38edceb45524d869bcd9a8b60069
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-21 21:19:35 -0300

    exemplo de código do resumo da aula

--- curso-java-udemy/src/aulas/EstruturaDeRepeticaoDoWhile.java
+++ curso-java-udemy/src/aulas/EstruturaDeRepeticaoDoWhile.java
@@ -19,7 +19,7 @@ public class EstruturaDeRepeticaoDoWhile {
 			double C = sc.nextDouble();
 			double F = 9.0 * C / 5.0 + 32.0;
 			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-			System.out.println("Deseja repetir (s/n)? ");
+			System.out.print("Deseja repetir (s/n)? ");
 			resp = sc.next().charAt(0);
 		} while(resp != 'n');
 		
