3789439b1691393b33b2ce107b4e10bb79d2dbf3
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-25 23:30:25 -0300

    adicionando descrição do exemplo

--- curso-java-udemy/src/aulas/segundoExOO/Program.java
+++ curso-java-udemy/src/aulas/segundoExOO/Program.java
@@ -4,6 +4,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
+	/*Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+	quantidade no estoque). Em seguida:
+	Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque)
+	Realizar uma entrada no estoque e mostrar novamente os dados do produto
+	Realizar uma saída no estoque e mostrar novamente os dados do produto*/
 
 	public static void main(String[] args) {
 		
