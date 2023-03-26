af381c9e9eaa5edf62f5bbf5f6beb2a37679f167
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-26 19:50:05 -0300

    Exercicio de fixacao

---
+++ curso-java-udemy/src/exercicios/orientacaoObjEx1/Rectangle.java
@@ -0,0 +1,19 @@
+package exercicios.orientacaoObjEx1;
+
+public class Rectangle {
+	public double width;
+	public double height;
+	
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return 2 * (width + height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(width * width + height * height);
+	}
+}
