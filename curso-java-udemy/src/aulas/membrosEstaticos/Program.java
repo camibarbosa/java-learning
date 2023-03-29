8d8ad751ffd88b6ea7dbfafac06c264aaea4df5f
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-03-29 00:23:38 -0300

    Problema exemplo de membro estáticos

---
+++ curso-java-udemy/src/aulas/membrosEstaticos/Program.java
@@ -0,0 +1,30 @@
+package aulas.membrosEstaticos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		/*Fazer um programa para ler um valor numérico qualquer, e daí mostrar 
+quanto seria o valor de uma circunferência e do volume de uma esfera 
+para um raio daquele valor. Informar também o valor de PI com duas 
+casas decimais*/
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circumference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
+		
+		sc.close();
+	}
+
+}
