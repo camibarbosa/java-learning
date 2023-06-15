56e93702a4c884a27b28aed6db2fe73d237b2262
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-06-14 21:59:00 -0300

    atualizando

--- curso-java-udemy/src/exercicios/vetores/application/Program.java
+++ curso-java-udemy/src/exercicios/vetores/application/Program.java
@@ -7,39 +7,14 @@ import exercicios.vetores.entities.Rent;
 
 public class Program {
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		Rent r = new Rent("Maria", "maria@gmail.com");
-		
-		Rent[] vect = new Rent[10];
-		
-		System.out.print("How many rooms will be rented? ");
-		int n = sc.nextInt();
-		
-		for(int i = 1; i <= n; i++) {
-			System.out.println();
-			System.out.println("Rent #" + i + ": ");
-			System.out.print("Name: ");
-			sc.nextLine();
-			String name = sc.nextLine();	
-			System.out.print("E-mail: ");
-			String email = sc.next();
-			System.out.println("Room: ");
-			int roomNum = sc.nextInt();
-			
-			//instanciando um obj e atribuindo uma referencia dele para um vetor na posicao roomNum
-			vect[roomNum] = new Rent(name, email);
-		}
-		
-		System.out.println();
-		System.out.println("Busy rooms: ");
-		for(int i = 0; i < 10; i++) {
-			if(vect[i] != null) {
-			System.out.println(i + ": " + vect[i]);
-			}
-		}
-			
-		sc.close();
+		 int n = 1;
+	        while (n < 20) {
+	            if (n%7 == 0) {
+	                continue;
+	            }
+	            System.out.println(n);
+	            n++;
+	       }
+	   }
 	}
-}
+
